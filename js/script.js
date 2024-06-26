0e4f373724a84144a903b6e8db9885e3e87f154f
mstD11 <4ekhof@gmail.com>
2024-06-26 03:31:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = new ItcSimpleSlider('#product-slider', {
+        loop: true,
+        autoplay: true,
+        interval: 3000,
+        swipe: true
+    });
+})
